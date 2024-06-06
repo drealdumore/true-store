fbe3ddff835b2ce7fd62a7fb1622de9346197264
drealdumore <sireroyce1@gmail.com>
2024-06-06 08:26:01 +0100

    dashboard && sidenav && navbar

--- src/app/admin/Layout.tsx
+++ src/app/admin/Layout.tsx
@@ -6,8 +6,7 @@ import Navbar from "@/components/admin/Navbar";
 import Sidebar from "@/components/admin/Sidebar";
 import { useAppSelector } from "@/redux/hooks";
 import { useSession } from "next-auth/react";
-import { usePathname } from "next/navigation";
-import React, { ReactNode, useEffect, useState } from "react";
+import React, { ReactNode, useEffect } from "react";
 
 interface LayoutProps {
   children: ReactNode;
@@ -16,8 +15,6 @@ interface LayoutProps {
 const Layout = ({ children }: LayoutProps) => {
   const isLoading = useAppSelector((store) => store.loadingReducer);
   const { data: session } = useSession();
-  const [searchTerm, setSearchTerm] = useState("");
-  const pathName = usePathname()
 
   // useEffect(() => {
   //   console.log("Redux loading state:", isLoading);
@@ -27,19 +24,12 @@ const Layout = ({ children }: LayoutProps) => {
   //   return <LoginPage />;
   // }
 
-  const handleSearch = (term: React.SetStateAction<string>) => {
-    setSearchTerm(term);
-  };
-
-  const isAdminProductsRoute = pathName === "/admin/products";
-
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar />
 
       <div className="w-full h-full flex flex-col">
-        {/* <Navbar /> */}
-        <Navbar onSearch={isAdminProductsRoute ? handleSearch : undefined} />
+        <Navbar />
 
         <div className="bg-muted/40 p-4 h-[calc(100vh-64px)] overflow-y-scroll">
           {children}
